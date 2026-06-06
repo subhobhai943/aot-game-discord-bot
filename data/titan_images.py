1f778d794a5222388d2505ce6546e59e6f2c691f
subhobhai943 <sarkarsubhadip604@gmail.com>
2026-06-06 19:22:44 +0530

    feat: initialize game data structure, add titan asset mappings, and update gitignore to track data files

---
+++ data/titan_images.py
@@ -0,0 +1,36 @@
+import os
+
+TITAN_IMAGES = {
+    "Attack Titan": ["assets/Titans/attack_titan.jpg"],
+    "Colossal Titan": ["assets/Titans/colossal_titan.jpg"],
+    "Armored Titan": ["assets/Titans/armored_titan.jpg"],
+    "Female Titan": ["assets/Titans/female_titan.jpg"],
+    "Beast Titan": ["assets/Titans/beast_titan.jpg"],
+    "Cart Titan": ["assets/Titans/cart_titan.jpg"],
+    "Jaw Titan": ["assets/Titans/jaw_titan.jpg"],
+    "War Hammer Titan": ["assets/Titans/war_hammer_titan.jpg"],
+    "Founding Titan": ["assets/Titans/founding_titan.jpg"],
+    "Pure Titan": ["assets/Titans/pure_titan.jpg"],
+    "Smiling Titan": ["assets/Titans/pure_titan.jpg"],
+    "Abnormal Titan": ["assets/Titans/abnormal_titan.jpg"],
+    "Transformation": ["assets/Titans/colossal_titan.jpg"]
+}
+
+SURVEY_CORPS_CHARACTERS = [
+    "Levi Ackerman",
+    "Mikasa Ackerman",
+    "Armin Arlert",
+    "Erwin Smith",
+    "Hange Zoë",
+    "Jean Kirstein",
+    "Connie Springer",
+    "Sasha Blouse",
+    "Historia Reiss",
+    "Petra Ral",
+    "Marco Bott",
+    "Moblit Berner"
+]
+
+SURVEY_CORPS_IMAGES = [
+    "assets/Titans/survey_corps.png"
+]
